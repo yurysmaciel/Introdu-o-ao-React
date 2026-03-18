6dff8429ff24f9eb3e39c79772947d79df6bf158
unknown <yurymaciel6371@digitalcollege.internal>
2026-03-17 22:00:31 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Button from "./components/button/button";
 export default function App() {
   return(
     <>
-    <h1>Olá, Tudo bem</h1>
+    <h1 className="bg-red-500">Olá, Tudo bem</h1>
     <p>Teste do projeto react</p>
     <Button text={"Salvar"}/>
     </>
